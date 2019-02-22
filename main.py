0729c3751e2b30f6f7a046746019d9712c2f4123
DoubleZ <zbludo@hotmail.com>
2019-02-22 11:11:56 +0100

    Ajout d'une nouvelle function

--- main.py
+++ main.py
@@ -27,3 +27,10 @@ def features_v3():
 	chaussette = "gauche"
 	chaussure = "droite"
 
+
+def nouveau_fonction_v3():
+	"""nouveau bouton menu"""
+
+	#nouvelle feature v3
+
+	#nouvelle modif pour featureV3/nouveau bouton
